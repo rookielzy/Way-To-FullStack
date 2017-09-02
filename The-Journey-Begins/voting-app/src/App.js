1504fd04e28b84d33c0918aa8f72d7e5936ed0ce
rookielzy <slimjetlzy@gmail.com>
2017-09-02 16:00:05 +0800

    Using Props Send Data

--- The-Journey-Begins/voting-app/src/App.js
+++ The-Journey-Begins/voting-app/src/App.js
@@ -1,32 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Seed from './js/seed'
 
 class ProductList extends React.Component {
   render() {
-      return (
-          <div className="ui unstackable items">
-              <Product />
-          </div>
-      )
+    const seed = new Seed()
+    const products = seed.products
+    // console.log(products.products, products.products[0].votes())
+    return (
+      <div className="ui items">
+        <Product
+          id={products[0].id}
+          title={products[0].title}
+          description={products[0].description}
+          url={products[0].url}
+          votes={products[0].votes}
+          submitterAvatarUrl={products[0].submitterAvatarUrl}
+          productImageUrl={products[0].productImageUrl}
+        />
+      </div>
+    )
   }
 }
 
 class Product extends React.Component {
   render() {
     return (
-      <div>
-        <div className="item">
-          <div className="ui small image">
-            <img src="images/products/image-aqua.png" />
+      <div className="item">
+        <div className="image">
+          <img src={this.props.productImageUrl} />
+        </div>
+        <div className="content">
+          <a className="header">
+            <i className="large caret up icon"></i>
+            {this.props.votes()}
+          </a>
+          <div className="meta">
+            <a>{this.props.title}</a>
           </div>
-          <div className="middle aligned content">
-            <div className="description">
-              <a>Fort Knight</a>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias ipsam quidem doloribus vitae tempore dolore?</p>
-            </div>
-            <div className="extra">
-              <span>Submitted by:</span>
-              <img src="images/avatars/daniel.jpg" className="ui avatar image" />
-            </div>
+          <div className="description">
+            <p>{this.props.description}</p>
+          </div>
+          <div className="extra">
+            <span>Submitted by:</span>
+            <img src={this.props.submitterAvatarUrl} className="ui avatar image" />
           </div>
         </div>
       </div>
