1c389af686c0677c880a7f8fd217fdeb9fed5150
rookielzy <slimjetlzy@gmail.com>
2017-09-11 18:04:22 +0800

    Dive in DOM Prevent Default Behavior

--- The-Journey-Begins/Dive-in-DOM/app.js
+++ The-Journey-Begins/Dive-in-DOM/app.js
@@ -17,6 +17,23 @@ list.addEventListener('click', function(e) {
 const addForm = document.forms['add-book'];
 addForm.addEventListener('submit', function(e) {
     e.preventDefault(); // Important!!! Prevent the page reload
-    const bookName = addForm.querySelector('input[type="text"]').value;
+    const value = addForm.querySelector('input[type="text"]').value;
     
+    // list.innerHTML += '<li><span class="name">' + value + '</span><span class="delete">delete</span></li>'; 
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    // add content
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
+
+    // style it
+    deleteBtn.classList.add('delete');
+    bookName.classList.add('name');
+
+    // append to list
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
 })
