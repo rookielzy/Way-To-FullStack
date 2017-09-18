d7118645e4eda21267dfcb2fac87b8b022dbc19a
rookielzy <slimjetlzy@gmail.com>
2017-09-19 00:15:02 +0800

    React Image Slider

---
+++ The-Journey-Begins/slider/src/seed.js
@@ -0,0 +1,29 @@
+const slides = [
+    {
+        city: '东京',
+        country: '日本',
+        img: './images/tokyo.jpeg',
+    },
+    {
+        city: '上海',
+        country: '中国',
+        img: './images/shanghai.jpeg',
+    },
+    {
+        city: '纽约',
+        country: '美国',
+        img: './images/newyork.jpeg',
+    },
+    {
+        city: '广州',
+        country: '中国',
+        img: './images/guangzhou.jpg',
+    },
+    {
+        city: '京都',
+        country: '日本',
+        img: './images/kyoto.jpeg',
+    },
+];
+
+export default slides;
