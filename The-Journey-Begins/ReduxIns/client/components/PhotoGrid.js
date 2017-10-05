222d5e4d7db5308abadcb35308b0ef8e48cdddae
rookielzy <slimjetlzy@gmail.com>
2017-10-05 16:05:46 +0800

    Basic Router

---
+++ The-Journey-Begins/ReduxIns/client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+    render() {
+        return (
+            <div className="photo-grid">
+                I am Photo Grid
+            </div>
+        );
+    }
+})
+
+export default PhotoGrid;
--- The-Journey-Begins/ReduxIns/client/reduxstagram.js
+++ The-Journey-Begins/ReduxIns/client/reduxstagram.js
@@ -1,1 +1,19 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+import css from './styles/style.styl';
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+const router = (
+    <Router history={browserHistory}>
+        <Route path="/" component={Main}>
+            <IndexRoute component={PhotoGrid} />
+            <Route path="/view/:postId" component={Single} />
+        </Route>
+    </Router>
+);
+
+render(router, document.getElementById('root'));
