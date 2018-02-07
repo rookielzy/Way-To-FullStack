246dc4c1b5ba3c7dc1b3259a2b7f5befd2a79230
rookielzy <slimjetlzy@gmail.com>
2018-02-07 14:16:42 +0800

    Easy Modal Component

---
+++ Vue/mt-food/src/components/modal/modal.vue
@@ -0,0 +1,99 @@
+<template>
+  <transition name="modal-fade">
+  <div class="modal">
+    <div class="test" @click="close"></div>
+    <div class="modal-dialog">
+      <div class="modal-header">
+        <slot name="header">I am the header</slot>
+        <button type="button" class="close-btn" @click="close"><span>&times;</span></button>
+      </div>
+      <div class="modal-body">
+        <slot name="body">I am the body</slot>
+      </div>
+      <div class="modal-footer">
+        <slot name="footer">I am the footer</slot>
+      </div>
+    </div>
+  </div>
+  </transition>
+</template>
+<script>
+export default {
+  name: 'modal',
+  methods: {
+    close () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style>
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.test {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  z-index: 99;
+}
+
+.modal-dialog {
+  position: absolute;
+  background-color: #fff;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-flow: column;
+  border-radius: 5px;
+  min-width: 200px;
+  padding: 5px 10px;
+  z-index: 999;
+}
+
+.modal-dialog > div {
+  width: 100%;
+}
+
+.modal-header {
+  position: relative;
+  min-height: 20px;
+}
+
+.close-btn {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+}
+
+.modal-body {
+  min-height: 100px;
+}
+
+.modal-footer {
+  min-height: 20px;
+}
+
+.modal-fade-enter-active, .modal-fade-leave-active {
+  transition: opacity .5s;
+}
+.modal-fade-enter, .modal-fade-leave-to {
+  opacity: 0;
+}
+</style>
--- Vue/mt-food/src/pages/comment/comment.vue
+++ Vue/mt-food/src/pages/comment/comment.vue
@@ -1,13 +1,31 @@
 <template>
   <div>
-    Comment
+    <button type="button" @click="showModal">Show Modal</button>
+    <modal v-show="isVisible" @close="closeModal">
+      <h1 slot="header">I am the new header</h1>
+    </modal>
   </div>
 </template>
 
 <script>
+import modal from '../../components/modal/modal'
+
 export default {
   data () {
-    return {}
+    return {
+      isVisible: false
+    }
+  },
+  components: {
+    modal
+  },
+  methods: {
+    showModal () {
+      this.isVisible = true
+    },
+    closeModal () {
+      this.isVisible = false
+    }
   }
 }
 </script>
