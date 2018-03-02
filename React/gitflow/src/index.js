5f4981538ec73002f76a5434e9a8a38c7f92ae0e
rookielzy <slimjetlzy@gmail.com>
2018-03-02 14:15:55 +0800

    add styled-component & global reset css

--- React/gitflow/src/index.js
+++ React/gitflow/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import globalStyle from './global-style'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
