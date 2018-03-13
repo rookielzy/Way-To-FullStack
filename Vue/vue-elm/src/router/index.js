64d775eaaf3288027064bb641ba9e5512061affc
rookielzy <slimjetlzy@gmail.com>
2018-03-13 23:57:38 +0800

    Add Some Simple Routes

--- Vue/vue-elm/src/router/index.js
+++ Vue/vue-elm/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '../components/hello.vue'
+
+const Home = r => require.ensure([], () => r(require('../components/home/home')), 'home')
+const City = r => require.ensure([], () => r(require('../components/city/city')), 'city')
+const Msite = r => require.ensure([], () => r(require('../components/msite/msite')), 'msite')
 
 Vue.use(Router)
 
@@ -8,8 +11,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/city',
+      name: 'City',
+      component: City
+    },
+    {
+      path: '/msite',
+      name: 'Msite',
+      component: Msite
     }
   ]
 })
