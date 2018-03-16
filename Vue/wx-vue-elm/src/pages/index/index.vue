cf51b00d5e8f1d13fe850b75bbc4cda3a61e85c3
rookielzy <slimjetlzy@gmail.com>
2018-03-16 15:44:43 +0800

    wx-vue-elm init

---
+++ Vue/wx-vue-elm/src/pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      motto: 'Hello World',
+      userInfo: {}
+    }
+  },
+
+  methods: {
+    
+  },
+
+  created () {
+    // 调用应用实例的方法获取全局数据
+    wx.login({
+      success: (res) => {
+        wx.getUserInfo({
+          success: (res) => {
+            this.userInfo = res.userInfo
+          }
+        })
+      }
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
